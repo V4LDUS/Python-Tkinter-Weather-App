e637190e6df53ea4c5cf65136b73696f9d5b79e3
Valdus <i.kabbash@protonmail.ch>
2022-01-01 15:44:24 +0200

    Update WeatherApp.py

--- WeatherApp.py
+++ WeatherApp.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from typing import Text
 
 HEIGHT = 700
 WIDTH = 700
@@ -16,7 +17,16 @@ frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
 # relative x and relative y to center the frame
 
 button = tk.Button(frame, text="Test Button", bg='crimson')  # Creating button
-button.pack()  # That will put the button on the screen inside the frame
+# That will put the button on the screen inside the frame
+button.place(relx=0, rely=0, relheight=0.25, relwidth=0.25)
+
+# 7th, label and entry
+label = tk.Label(frame, text="I'm a label", bg='lightyellow')
+label.pack(side='right', fill='y')
+
+entry = tk.Entry(frame, bg='lightgray')  # entry is where you type an input
+entry.pack(side='right', fill='y')  # change position and size by pack
+# can use .grid() or .place() instead
 
 root.mainloop()
 # Anything that we put in the app will be run between these two lines of code
