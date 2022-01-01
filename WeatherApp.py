0976dcca30f6bf4864c916221cf8b9803dbfba79
Valdus <i.kabbash@protonmail.ch>
2022-01-01 15:12:25 +0200

    Create WeatherApp.py

---
+++ WeatherApp.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+# Anything that we put in the app will be run between these two lines of code
+root = tk.Tk()
+
+button = tk.Button(root, text="Test Button", bg='crimson')  # Creating button
+button.pack()  # That will put the button on the screen
+
+root.mainloop()
+# Anything that we put in the app will be run between these two lines of code
