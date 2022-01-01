186480057a31609f1c6360e0c2f039dde067b84f
Valdus <i.kabbash@protonmail.ch>
2022-01-01 15:19:55 +0200

    Update WeatherApp.py

--- WeatherApp.py
+++ WeatherApp.py
@@ -1,10 +1,22 @@
 import tkinter as tk
 
+HEIGHT = 700
+WIDTH = 700
+
 # Anything that we put in the app will be run between these two lines of code
 root = tk.Tk()
 
-button = tk.Button(root, text="Test Button", bg='crimson')  # Creating button
-button.pack()  # That will put the button on the screen
+# 5th, we created a canva that'll be in the root
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+# 6th, a frame inside the canva
+frame = tk.Frame(root, bg='darkgreen')
+frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
+# relative x and relative y to center the frame
+
+button = tk.Button(frame, text="Test Button", bg='crimson')  # Creating button
+button.pack()  # That will put the button on the screen inside the frame
 
 root.mainloop()
 # Anything that we put in the app will be run between these two lines of code
